385e43baf95913024d4d4b66a0da12fabf40c541
Caio <caiohbda777@gmail.com>
2025-04-05 15:59:05 -0300

    index refatorado

--- src/index.js
+++ src/index.js
@@ -2,33 +2,19 @@ require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
-const { Either } = require('fp-ts/Either');
-const { pipe } = require('fp-ts/function');
-const { fold } = require('fp-ts/Either');
 
 // Repositories
 const MongoEmployeeRepository = require('./infrastructure/repositories/MongoEmployeeRepository');
 const MongoJourneyRepository = require('./infrastructure/repositories/MongoJourneyRepository');
 const MongoEmployeeJourneyRepository = require('./infrastructure/repositories/MongoEmployeeJourneyRepository');
 
-// Services
-const ActionService = require('./infrastructure/services/__mocks__/ActionService');
-const RedisQueue = require('./infrastructure/queue/__mocks__/RedisQueue');
-
 // Use Cases
 const AssociateJourneyToEmployee = require('./application/use-cases/AssociateJourneyToEmployee');
-const ExecuteJourneyAction = require('./application/use-cases/ExecuteJourneyAction');
 
 // Controllers
 const EmployeeController = require('./infrastructure/api/controllers/EmployeeController');
 const JourneyController = require('./infrastructure/api/controllers/JourneyController');
 const EmployeeJourneyController = require('./infrastructure/api/controllers/EmployeeJourneyController');
-
-// Middleware
-const { validateAssociateJourney } = require('./infrastructure/api/middlewares/validation');
-
-// Background Jobs
-const { setupActionProcessor } = require('./infrastructure/queue/ActionProcessor');
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -47,21 +33,11 @@ const employeeRepository = MongoEmployeeRepository();
 const journeyRepository = MongoJourneyRepository();
 const employeeJourneyRepository = MongoEmployeeJourneyRepository();
 
-// Services
-const actionService = ActionService();
-const journeyActionQueue = RedisQueue();
-
 // Use Cases
 const associateJourneyToEmployee = AssociateJourneyToEmployee({
   employeeRepository,
   journeyRepository,
   employeeJourneyRepository
-});
-
-const executeJourneyAction = ExecuteJourneyAction({
-  employeeJourneyRepository,
-  journeyRepository,
-  actionService
 });
 
 // Controllers
@@ -75,8 +51,7 @@ const journeyController = JourneyController({
 
 const employeeJourneyController = EmployeeJourneyController({
   associateJourneyToEmployee,
-  employeeJourneyRepository,
-  journeyActionQueue
+  employeeJourneyRepository
 });
 
 // Routes
@@ -91,16 +66,8 @@ app.get('/api/journeys', journeyController.getJourneys);
 app.get('/api/journeys/:id', journeyController.getJourney);
 
 // Employee Journey routes
-app.post('/api/employee-journeys', validateAssociateJourney, employeeJourneyController.associateJourney);
+app.post('/api/employee-journeys', employeeJourneyController.associateJourney);
 app.get('/api/employee-journeys/:employeeId', employeeJourneyController.getEmployeeJourneys);
-
-// Background Jobs
-setupActionProcessor({
-  employeeJourneyRepository,
-  journeyRepository,
-  actionService,
-  journeyActionQueue
-});
 
 // Error handling middleware
 app.use((err, req, res, next) => {
